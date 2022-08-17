2eb142902a980983a030c06ff79271dd37592fac
Atul-Acharya-17 <adiatul2001@gmail.com>
2022-08-18 02:19:03 +0800

    Refactored

--- src/components/window.jsx
+++ src/components/window.jsx
@@ -60,10 +60,7 @@ class Window extends Component {
         return array;
     }
 
-    handleBubble = () => {
-        console.log("Bubble sort");
-        const animations = bubbleSort(this.state.array);
-        const arrayBars = document.getElementsByClassName('array-bar');
+    createaAnimations = (animations, arrayBars, multiplier) => {
         for (let i = 0; i < animations.length; i++) {
             const [index1, index2, color, height1, height2] = animations[i];
             const barOneStyle = arrayBars[index1].style;
@@ -75,47 +72,29 @@ class Window extends Component {
                     barOneStyle.height = `${height1}px`;
                     barTwoStyle.height = `${height2}px`;
                 }
-            }, i*1);
+            }, i*multiplier);
         }
+    }
+
+    handleBubble = () => {
+        console.log("Bubble sort");
+        const animations = bubbleSort(this.state.array);
+        const arrayBars = document.getElementsByClassName('array-bar');
+        this.createaAnimations(animations, arrayBars, 1);
     }
 
     handleSelection = () => {
         console.log("Selection sort");
         const arrayBars = document.getElementsByClassName('array-bar');
         const animations = selectionSort(this.state.array);
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                    barTwoStyle.height = `${height2}px`;
-                }
-            }, i*20);
-        }
+        this.createaAnimations(animations, arrayBars, 20);
     }
 
     handleInsertion = () => {
         console.log("Insertion sort");
         const animations = insertionSort(this.state.array);
         const arrayBars = document.getElementsByClassName('array-bar');
-
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                    barTwoStyle.height = `${height2}px`;
-                }
-            }, i*1);
-        }
+        this.createaAnimations(animations, arrayBars, 1);
         
     }
 
@@ -123,19 +102,7 @@ class Window extends Component {
         console.log("Merge sort");
         const animations = mergeSort(this.state.array);
         const arrayBars = document.getElementsByClassName('array-bar');
-
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                }
-            }, i*1);
-        }
+        this.createaAnimations(animations, arrayBars, 1);
     }
 
     handleQuick = () => {
@@ -143,57 +110,21 @@ class Window extends Component {
         const animations = quickSort(this.state.array);
         const arrayBars = document.getElementsByClassName('array-bar');
 
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                    barTwoStyle.height = `${height2}px`;   
-                }
-            }, i*5);
-        }
+        this.createaAnimations(animations, arrayBars, 5);
     }
 
     handleHeap = () => {
         console.log("Heap sort");
         const animations = heapSort(this.state.array);
         const arrayBars = document.getElementsByClassName('array-bar');
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                    barTwoStyle.height = `${height2}px`;   
-                }
-            }, i*5);
-        }
+        this.createaAnimations(animations, arrayBars, 5);
     }
 
     handleBucket = () => {
         console.log("Bucket sort");
         const animations = bucketSort(this.state.array);
         const arrayBars = document.getElementsByClassName('array-bar');
-        for (let i = 0; i < animations.length; i++) {
-            const [index1, index2, color, height1, height2] = animations[i];
-            const barOneStyle = arrayBars[index1].style;
-            const barTwoStyle = arrayBars[index2].style;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-                if (color == secondColor) {
-                    barOneStyle.height = `${height1}px`;
-                    barTwoStyle.height = `${height2}px`;   
-                }
-            }, i*5);
-        }
+        this.createaAnimations(animations, arrayBars, 5);
     }
 
 }
